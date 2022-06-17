8d9343a23f08388b81a3c3490f1eda40ac027c46
Wislanildo Dias <wislanildo.dias@paygo-consult.com.br>
2022-06-17 16:45:27 -0300

    Testes de integração para creação, alteração, deleção de chaves pix.O teste da busca de chaves pix está com erro no assert.

--- src/main/java/com/bank/pixkeysmanager/keymanager/adapters/api/response/CreateKeyResponse.java
+++ src/main/java/com/bank/pixkeysmanager/keymanager/adapters/api/response/CreateKeyResponse.java
@@ -1,15 +1,25 @@
 package com.bank.pixkeysmanager.keymanager.adapters.api.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class CreateKeyResponse {
 
     @JsonProperty("id")
     private final UUID id;
+
+    CreateKeyResponse(@JsonProperty("id") final String id) {
+        this.id = UUID.fromString(id);
+    }
 
     public static CreateKeyResponse of(final UUID id) {
         return CreateKeyResponse.builder().id(id).build();
